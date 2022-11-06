4a3d312a3f38df67966234c2e9dd53f0093595d1
Maxim Ageev <maxim.ageev@paxful.com>
2022-11-06 16:04:40 +0300

    disttibution of x

--- vesualisation.py
+++ vesualisation.py
@@ -16,6 +16,19 @@ estimate_execution_time = False
 quantum_a = 7 ** 5
 quantum_m = 2 ** 31 - 1
 
+def drawDistribution(map, min, max):
+    # plt.xlabel('value')
+    # plt.ylabel('number')
+
+    # plt.axis([min, max, 0, M])
+
+    x = [*map.keys()]
+    y = [*map.values()]
+    print(x)
+    print(y)
+    plt.plot(x, y, 'ro')
+    plt.show()
+
 
 def analyze_trimpazation(n, m, q0):
     '''
@@ -26,48 +39,54 @@ def analyze_trimpazation(n, m, q0):
     at the top of the file.
     '''
     m_div2 = m // 2
+    min = -m_div2
+    max = m_div2
     q = q0
+    if m % 2 == 1:
+        max +=1
 
     # generating x data:
-    x = []
-    y = []
+
+    distribution_of_x = dict.fromkeys((range(min, max)), 0)
+
+
     for i in range(n):
+        # performanceStart('x_i = q % m - m_div2')
         x_i = q % m - m_div2
-        x.append(x_i)
-        y.append(i)
+        # performanceEnd('x_i = q % m - m_div2')
+        # performanceStart('q = ((q * quantum_a) % quantum_m)')
         q = ((q * quantum_a) % quantum_m)
+        # performanceEnd('q = ((q * quantum_a) % quantum_m)')
+        # performanceStart('insert hashmap')
+        distribution_of_x[x_i] += 1
+        # performanceEnd('insert hashmap')
 
-    # plotting the points
-    # plt.plot(y, x)
-
-    # naming the x axis
-    plt.xlabel('value')
-    # naming the y axis
-    plt.ylabel('index')
-
-    # giving a title to my graph
-    plt.title('My first graph!')
-
-    # function to show the plot
-
-    # sorting x data:
-    x.sort()
-    # plt.plot(y, x)
-    plt.plot(x, y)
-    plt.show()
-
-
+    step = 1
+    every = min
+    for key in distribution_of_x:
+        if key == every:
+            print(f'{key}:\t {distribution_of_x[key]}')
+            every += step
 
     # calculating sum:
     res = 0
-    for i, v in enumerate(x):
-        res += (i + 1) * v
+    i = 0
+    for v in range(min, max):
+        if v in distribution_of_x:
+            for _ in range(distribution_of_x[v]):
+                res += (i + 1) * v
+                i += 1
+
+
+    drawDistribution(distribution_of_x, min, max)
+
+
 
     return res
 
 
 if __name__ == '__main__':
-    N, M, q0 = [10000000, 10000, 1]
+    N, M, q0 = [10000000, 5, 1]
     t = time()
     print(analyze_trimpazation(N, M, q0))
     if estimate_execution_time:
